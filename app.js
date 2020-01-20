6415dc879fd4fb161f90eed6baa297f15ec1a41d
NV Lounge <careyy301@gmail.com>
2020-01-20 13:08:55 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,108 @@
+function init() {
+  const slides = document.querySelectorAll(".slide");
+  const pages = document.querySelectorAll(".page");
+
+  //  tracker
+  let current = 0;
+  let scrollSlide = 0;
+
+  slides.forEach((slide, index) => {
+    slide.addEventListener("click", function() {
+      changeDots(this);
+      nextSlide(index);
+      scrollSlide = index;
+    });
+  });
+
+  function changeDots(dot) {
+    slides.forEach(slide => {
+      slide.classList.remove("active");
+    });
+    dot.classList.add("active");
+  }
+
+  function nextSlide(pageNumber) {
+    const nextPage = pages[pageNumber];
+    const currentPage = pages[current];
+    const nextImg = nextPage.querySelector(".hero .model");
+    const currentImg = currentPage.querySelector(".hero .model");
+    const nextText = nextPage.querySelector(".details");
+    const portfolio = document.querySelector(".portfolio");
+
+    const tl = new TimelineMax({
+      onStart: function() {
+        slides.forEach(slide => {
+          slide.style.pointerEvents = "none";
+        });
+      },
+      onComplete: function() {
+        slides.forEach(slide => {
+          slide.style.pointerEvents = "all";
+        });
+      }
+    });
+
+    tl.fromTo(currentImg, 0.3, { y: "-10%" }, { y: "-100%" })
+      .fromTo(
+        currentPage,
+        0.3,
+        { opacity: 1, pointerEvents: "all" },
+        { opacity: 0, pointerEvents: "none" }
+      )
+      .fromTo(
+        nextPage,
+        0.3,
+        { opacity: 0, pointerEvents: "none" },
+        { opacity: 1, pointerEvents: "all" },
+        "-=0.1"
+      )
+      .fromTo(nextImg, 0.3, { y: "-100%" }, { y: "1.8%" }, "-=0.6")
+      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+      .set(nextImg, { clearProps: "all" });
+
+    current = pageNumber;
+  }
+
+  document.addEventListener("wheel", throttle(scrollChange, 1500));
+  document.addEventListener("touchmove", throttle(scrollChange, 1500));
+
+  function switchDots(dotNumber) {
+    const activeDot = document.querySelectorAll(".slide")[dotNumber];
+    slides.forEach(slide => {
+      slide.classList.remove("active");
+    });
+    activeDot.classList.add("active");
+  }
+
+  function scrollChange(e) {
+    if (e.deltaY > 0) {
+      scrollSlide += 1;
+    } else {
+      scrollSlide -= 1;
+    }
+
+    if (scrollSlide > 2) {
+      scrollSlide = 0;
+    }
+    if (scrollSlide < 0) {
+      scrollSlide = 2;
+    }
+    switchDots(scrollSlide);
+    nextSlide(scrollSlide);
+  }
+}
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+init();
